b0d76270f600b08e375eee28c016fd33bfad4175
Tino <tinomuzambi@gmail.com>
2019-10-07 16:06:10 +0200

    Updated algorithm for replication testing.

--- driver.py
+++ driver.py
@@ -15,21 +15,29 @@ def generate_list(size):
 
 
 def main():
-    for i in range(10000, 50001, 10000):
-        #print("Generating list...")
-        arr = generate_list(i)
-        #print("Done.")
-        #print("Starting timer...")
-        curr_time = time.time()
-        #print("Starting sort...")
-        # quickSort(arr, 0, size - 1)
-        do_selection_sort(arr)
-        #print("Done.")
-        stop_time = time.time()
-        #print("Stopped timer.")
-        taken = stop_time - curr_time
-        # print(arr)
-        print("{:<5} elements took {:10.6f} seconds.".format(i, taken))
+    results = open("results.txt","w")
+    for j in range(9):
+        print("run", j + 1)
+        for i in range(10000, 50001, 10000):
+            print("Generating list", i, "...")
+            arr = generate_list(i)
+            print("Done.")
+            print("Starting timer...")
+            curr_time = time.time()
+            print("Starting sort...")
+            # quickSort(arr, 0, size - 1)
+            do_selection_sort(arr)
+            print("Done.")
+            stop_time = time.time()
+            print("Stopped timer.")
+            taken = stop_time - curr_time
+            # print(arr)
+            print("{:<5} elements took {:10.6f} seconds.".format(i, taken), file = results)
+        print("", file = results)
+    results.close()
+    print("\a")  # Make sound when done.
+    print("\a")  # Make sound when done.
+    print("\a")  # Make sound when done.
 
 
 if __name__ == '__main__':
